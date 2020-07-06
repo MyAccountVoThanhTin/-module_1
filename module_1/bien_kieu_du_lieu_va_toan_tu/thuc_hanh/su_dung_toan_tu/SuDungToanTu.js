f036ea7e16d2d45f4657080e2f3435f37757ae37
unknown <you@example.com>
2020-07-06 20:36:01 +0700

    add

---
+++ module_1/bien_kieu_du_lieu_va_toan_tu/thuc_hanh/su_dung_toan_tu/SuDungToanTu.js
@@ -0,0 +1,8 @@
+function dienTich() {
+    var inputWidth;
+    var inputHeight;
+    inputWidth = prompt("Nhập chiều rộng");
+    inputHeight = prompt("Nhập chiều dài");
+    var dienTich = inputWidth * inputHeight;
+    document.write("Diện tích hình chữ nhật là : " +dienTich);
+}
